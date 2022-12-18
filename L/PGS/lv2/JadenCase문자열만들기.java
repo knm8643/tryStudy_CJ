8f73b99facafa09c7b6120d56508378984c46665
이충재 <knm8643@nate.com>
2022-12-18 13:06:30 +0900

    [PGS] JadenCase문자열만들기 / lv2 / 90분 /

--- L/PGS/lv2/JadenCase문자열만들기.java
+++ L/PGS/lv2/JadenCase문자열만들기.java
@@ -4,25 +4,43 @@ public class JadenCase문자열만들기 {
     class Solution {
         public String solution(String s) {
             String answer = "";
-            String sibalAnswer = "";
             String Array[] = s.split(" ");
 
             for(int i=0; i<Array.length; i++){
-                sibalAnswer = Array[i].substring(0,1).toUpperCase();
+                if(Array[i].length() == 0) answer += " ";
+                else {
+                    answer += Array[i].substring(0, 1).toUpperCase();
+                    answer += Array[i].substring(1, Array[i].length()).toLowerCase();
+                    answer += " ";
 
-                if(Array[i].substring(0,1) == sibalAnswer){
-                    System.out.println("앞글자 대문자 없음");
-                }else {
-                    if((i+1)<Array.length) {
-                        answer += Array[i].toLowerCase() + " ";
-                    } else {
-                        answer += Array[i].toLowerCase();
-                        answer = Array[i].repl
-                        // toUpperCase()
-                    }
+                    // Array[i] = Array[i].toLowerCase();
+                    // Array[i] = Array[i].replaceAll(Array[i].substring(0,1),Array[i].substring(0,1).toUpperCase());
+                    // answer += Array[i] + " ";
                 }
             }
-            return answer;
+            // answer = answer.substring(0, answer.length() - 1);
+            // 테스트 8케이스 이해 못하겠네
+            if(s.substring(s.length() -1, s.length()).equals(" ")) return answer;
+            return answer.substring(0, answer.length() - 1);
+
+//         ----------------------------------- 첫 번째 시도 ------------------------------
+//         String sibalAnswer = "";
+//         for(int i=0; i<Array.length; i++){
+//             sibalAnswer = Array[i].substring(0,1).toUpperCase();
+
+//             if(Array[i].substring(0,1) == sibalAnswer){
+//                 System.out.println("앞글자에 대문자 없음");
+//             }else {
+//                 Array[i] = Array[i].toLowerCase();
+//                 Array[i] = Array[i].replaceAll(Array[i].substring(0,1),sibalAnswer);
+//                 if((i+1)<Array.length) {
+//                     answer += Array[i] + " ";
+//                 } else {
+//                     answer += Array[i];
+//                 }
+//             }
+//         }
+//         return answer;
         }
     }
 }
